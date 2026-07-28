fc38b2902a51b83d6726320c7714ff1f535a15a1
valiant1412 <85612329+valiant1412@users.noreply.github.com>
2026-07-29 01:13:10 +0700

    add reset buttpn

---
+++ Assets/Scripts/Button/ButtonController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public void ResetBtn()
+    {
+        SceneManager.LoadScene("Location soccer field");
+    }
+}
